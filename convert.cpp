ab954981166e4561b4659e9f58c3ac39125f19d3
HyperTT <HyperTT@users.noreply.github.com>
2017-07-02 23:20:34 -0700

    Create convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+float convert_to_fah(float celsius)
+{
+    //calculate celsius f = c * 1.8 + 32
+    celsius = celsius * 1.8 + 32;
+    return celsius;
+}
